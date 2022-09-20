8c640f1a5f41f0d1a7a0d1e6cc422591f8aafcc2
Jakub Łata <lootjakub@gmail.com>
2022-09-20 18:31:20 +0200

    first commit

---
+++ network/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post ({self.user.username}) #{self.id}"
+    
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "user": self.user,
+    #         "timestamp": self.timestamp
+    #     }
+
+class Comment(models.Model):
+    pass
+
+class Follower(models.Model):
+    pass
